ec09028f847cb87c4a8955d252541837513b0f70
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-03-08 01:03:24 -0500

    a

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,23 @@
-// TODO: Create action creators as defined in tests
+export const ADD_QUOTE = "ADD_QUOTE"
+export const addQuote = (quote) => {
+    return {
+        type: ADD_QUOTE, 
+        quote
+    }
+}
+
+export const REMOVE_QUOTE = "REMOVE_QUOTE";
+export const removeQuote = (quote) => {
+  return {
+    type: REMOVE_QUOTE,
+    quote
+  };
+};
+
+export const UPDATE_QUOTE = "UPDATE_QUOTE";
+export const updateQuote = (quote) => {
+  return {
+    type: UPDATE_QUOTE,
+    quote
+  };
+};
