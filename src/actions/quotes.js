228648ac101104349ec45f38fb67e6ac44554557
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-03-09 03:41:57 -0500

    ok

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,8 +1,9 @@
-import uuid from "uuid";
-export const ADD_QUOTE = "ADD_QUOTE";
-export const REMOVE_QUOTE = "REMOVE_QUOTE";
-export const UPVOTE_QUOTE = "UPVOTE_QUOTE";
-export const DOWNVOTE_QUOTE = "DOWNVOTE_QUOTE";
+
+import uuid from 'uuid';
+export const ADD_QUOTE = 'ADD_QUOTE'
+export const REMOVE_QUOTE = 'REMOVE_QUOTE'
+export const UPVOTE_QUOTE = 'UPVOTE_QUOTE'
+export const DOWNVOTE_QUOTE = 'DOWNVOTE_QUOTE'
 // TODO: Create action creators as defined in tests
 export const addQuote = (quote) => {
   let id = uuid();
@@ -11,28 +12,28 @@ export const addQuote = (quote) => {
     quote: {
       id,
       ...quote,
-      votes: 0,
-    },
-  };
-};
+      votes: 0
+    }
+  }
+}
 
 export const removeQuote = (quoteId) => {
   return {
     type: REMOVE_QUOTE,
-    quoteId,
+    quoteId
   };
 };
 
 export const upvoteQuote = (quoteId) => {
   return {
     type: UPVOTE_QUOTE,
-    quoteId,
+    quoteId
   };
 };
 
 export const downvoteQuote = (quoteId) => {
   return {
     type: DOWNVOTE_QUOTE,
-    quoteId,
+    quoteId
   };
 };
